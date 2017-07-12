fd8a9793300d70206dfae3df29834d06083ad8df
Matt Cree <matthew.cree@gmail.com>
2017-07-12 13:44:06 +0100

    changed to spaces

--- MyEnum.exs
+++ MyEnum.exs
@@ -1,85 +1,85 @@
 defmodule MyEnum do
-	
-	def all?(list, func \\ fn x -> x end)
+  
+  def all?(list, func \\ fn x -> x end)
 
-	def all?([], _func) do
-		true
-	end
+  def all?([], _func) do
+    true
+  end
 
-	def all?([head|tail], func) do
-		if func.(head) do
-			all?(tail, func)
-		else
-			false
-		end
-	end
+  def all?([head|tail], func) do
+    if func.(head) do
+      all?(tail, func)
+    else
+      false
+    end
+  end
 
-	def each([], _func) do
-		:ok
-	end
-
-
-	def each([head|tail], func) do
-		func.(head)
-		each(tail, func)
-	end
-
-	def filter(list, func) do
-		_do_filter(list, func, [])
-	end
-
-	defp _do_filter([], _func, list) do
-		Enum.reverse list
-	end
-
-	defp _do_filter([head|tail], func, list) do
-		if func.(head) do
-			_do_filter(tail, func, [head|list])
-		else
-			_do_filter(tail, func, list)
-		end
-	end
+  def each([], _func) do
+    :ok
+  end
 
 
-	def split(list, 0) do
-		{[], list}
-	end
+  def each([head|tail], func) do
+    func.(head)
+    each(tail, func)
+  end
 
-	def take(_list, 0) do
-		[]
-	end
+  def filter(list, func) do
+    _do_filter(list, func, [])
+  end
 
-	def take([head|tail], count) when count >= 1 do
-		_do_take(tail, count-1, [head], :positive)
-	end
+  defp _do_filter([], _func, list) do
+    Enum.reverse list
+  end
 
-	def take(list, count) when count <= -1 do
-		[head|tail] = Enum.reverse list
-		_do_take(tail, -count-1, [head], :negative)
-	end
+  defp _do_filter([head|tail], func, list) do
+    if func.(head) do
+      _do_filter(tail, func, [head|list])
+    else
+      _do_filter(tail, func, list)
+    end
+  end
 
-	defp _do_take(list, count, taken, direction) when count == 0 or list == [] do
-		if direction === :positive do
-			Enum.reverse taken
-		else
-			taken
-		end
-	end
 
-	defp _do_take([head|tail], count, taken, direction) do
-		_do_take(tail, count-1, [head|taken], direction)
-	end
+  def split(list, 0) do
+    {[], list}
+  end
 
-	def len([_head|tail]) do
-		_do_len(tail, 1)
-	end
+  def take(_list, 0) do
+    []
+  end
 
-	defp _do_len([], count) do
-		count
-	end
+  def take([head|tail], count) when count >= 1 do
+    _do_take(tail, count-1, [head], :positive)
+  end
 
-	defp _do_len([_head|tail], count) do
-		_do_len(tail, count+1)
-	end
+  def take(list, count) when count <= -1 do
+    [head|tail] = Enum.reverse list
+    _do_take(tail, -count-1, [head], :negative)
+  end
+
+  defp _do_take(list, count, taken, direction) when count == 0 or list == [] do
+    if direction === :positive do
+      Enum.reverse taken
+    else
+      taken
+    end
+  end
+
+  defp _do_take([head|tail], count, taken, direction) do
+    _do_take(tail, count-1, [head|taken], direction)
+  end
+
+  def len([_head|tail]) do
+    _do_len(tail, 1)
+  end
+
+  defp _do_len([], count) do
+    count
+  end
+
+  defp _do_len([_head|tail], count) do
+    _do_len(tail, count+1)
+  end
 
 end
